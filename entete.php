e33603a8c4536320f883e9538595e8fb577fb4c1
JulieViolette <81251298+JulieViolette@users.noreply.github.com>
2021-04-02 22:19:56 +0200

    Add files via upload

---
+++ entete.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<head>
+	<meta charset="UTF-8" />
+	<title>Terre et Bois</title>
+	<link rel="stylesheet" href="styleentete.css"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600&display=swap" rel="stylesheet">
+</head>
+<body>
+<div id='bkgtitre'><div id='titrefond'><div id='titre'><h1>TERRE<br>&<br>BOIS</h1></div></div></div>
+<div id='caption'><h3>Grès d'art et marqueterie, par Sylviane et Jean Jacques Cleret</h3></div>
+<section id='nav'>
+    <a href='index.php'>Accueil</a>
+    <a href='gres.html'>Grès</a>
+    <a href='marque.html'>Marqueterie</a>
+    <a href='connexion.html'>Connexion</a>
+
+</body>
+</html>
